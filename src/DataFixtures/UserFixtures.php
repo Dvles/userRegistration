bbc9e6c45365c85847ae7ea9af31f5077e556714
leslie <l.habimana@interface3.be>
2024-09-18 11:44:57 +0200

    hashed password via dependency

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,30 +5,46 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private $passwordHasher;
+
+    // Correct constructor method with dependency injection
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        for ($i=0; $i<5; $i++){
+
+
+        for ($i = 0; $i < 5; $i++) {
             $user = new User();
             $user->setEmail("user" . $i . "@gmail.com");
-            $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
-            $user->setPassword("0000");
-            $manager->persist($user);
-        }
-        
+            $user->setRoles(['ROLE_USER']);
 
-        for ($i=0; $i<5; $i++){
-            $user = new User();
-            $user->setEmail("admin" . $i . "@gmail.com");
-            $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
-            $user->setPassword("0000");
+            // Hash the password using injected hasher
+            $hashedPassword = $this->passwordHasher->hashPassword($user, '0000');
+            $user->setPassword($hashedPassword);
+
             $manager->persist($user);
         }
-        
+
+        for ($i = 0; $i < 5; $i++) {
+            $admin = new User();
+            $admin->setEmail("admin" . $i . "@gmail.com");
+            $admin->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+
+            // Hash the password for admins
+            $hashedPassword = $this->passwordHasher->hashPassword($admin, '0000');
+            $admin->setPassword($hashedPassword);
+
+            $manager->persist($admin);
+        }
+
         $manager->flush();
     }
 }
